e643ce65c52bdcfaae7025b2e62e157c2d74eec5
Aashish Panthi <aashishpanthi11@gmail.com>
2022-12-15 22:38:47 +0545

    calculate distance

---
+++ ultrasonic_ino/ultrasonic_ino.ino
@@ -0,0 +1,28 @@
+int trigpin = 8;
+int echopin = 7;
+
+long duration;
+int distance;
+
+void setup() {
+  pinMode(trigpin, OUTPUT);
+  pinMode(echopin, INPUT);
+  Serial.begin(9600);
+  delay(50);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(trigpin, LOW);
+  delay(1);
+
+  digitalWrite(trigpin, HIGH);
+  delay(20);
+  digitalWrite(trigpin, LOW);
+
+  duration = pulseIn(echopin, HIGH);
+  distance = duration * 0.034/2;
+
+  Serial.print("Distance: ");
+  Serial.println(distance);
+}
